a7cc92cbe924def3b715211f51640a6d706df1fc
gyurika <gyorgykoszegi39@gmail.com>
2024-06-04 11:52:19 +0200

    enable endpoints/controllers

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 import matplotlib
-#from controllers.portfolio_controller import router as portfolio_router
+from controllers.portfolio_controller import router as portfolio_router
 
 app = FastAPI()
 matplotlib.use('Agg')
@@ -19,7 +19,7 @@ app.add_middleware(
 def read_root():
     return {"Good luck!": "Have a nice day :)"}
 
-#app.include_router(portfolio_router)
+app.include_router(portfolio_router)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
